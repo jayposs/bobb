92243e569b0311fbe56015bc7a3290f10befaed3
jayposs <jayposs88@outlook.com>
2026-06-07 21:30:13 -0500

    func CsvDataRec return type instance not ptr

--- datatypes/csvexport.go
+++ datatypes/csvexport.go
@@ -7,6 +7,8 @@ import (
 	"github.com/jayposs/bobb"
 )
 
+// funcs used by exportcsv/exportcsv.go pgm
+
 func toCsvExport(rec []byte, dataRec any) bobb.CsvExport {
 	if err := json.Unmarshal(rec, dataRec); err != nil {
 		log.Println("toCsvExport ", err)
@@ -16,10 +18,11 @@ func toCsvExport(rec []byte, dataRec any) bobb.CsvExport {
 
 // CsvDataRec unMarshals jsonBytes into instance of specific dataType based on bktName.
 // All dataTypes must implement the bobb.CsvExport interface (see types.go).
+// NOTE - this func is custom per project, using its bucket names and datatypes
 func CsvDataRec(rec []byte, bktName string) bobb.CsvExport {
 	switch bktName {
 	case "location":
-		return toCsvExport(rec, &Location{})
+		return toCsvExport(rec, Location{})
 	}
 	return nil
 }
