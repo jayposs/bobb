54771c3861c60ecaac5208549233f065d07161f7
jayposs <jayposs88@outlook.com>
2026-06-14 11:17:58 -0500

    use gzipReader pool,  json.Decode resp

--- client/client.go
+++ client/client.go
@@ -10,6 +10,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/jayposs/bobb"
@@ -20,6 +21,25 @@ var (
 	BaseURL string // port must match value in settings file used by bobb_server
 	Debug   bool
 )
+
+// A minimal, valid gzip string (empty content compressed)
+// used to safely initialize new readers in the pool.
+var emptyGzipBytes = []byte{
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0xff, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00,
+}
+
+var gzipReaderPool = sync.Pool{
+	New: func() any {
+		// Initialize the reader with a dummy valid gzip header
+		r, err := gzip.NewReader(bytes.NewReader(emptyGzipBytes))
+		if err != nil {
+			panic(err)
+		}
+		return r
+	},
+}
 
 // Run sends bobb server http request using the provided payload.
 // Used by client Go programs to interact with db.
@@ -56,12 +76,14 @@ func Run(httpClient *http.Client, op string, payload any) (*bobb.Response, error
 		log.Println("client.Run, http.Do request failed", err)
 		return nil, err
 	}
+
 	defer func() {
 		if requestFailed {
-			io.Copy(io.Discard, httpResp.Body) // drain body to allow connection reuse
+			io.Copy(io.Discard, httpResp.Body) // ensure body is drained to allow connection reuse
 		}
 		httpResp.Body.Close()
 	}()
+
 	if httpResp.StatusCode != http.StatusOK {
 		requestFailed = true
 		log.Println("Request Failed, Status:", httpResp.Status)
@@ -71,31 +93,50 @@ func Run(httpClient *http.Client, op string, payload any) (*bobb.Response, error
 		return nil, errors.New("bad http response Status-" + httpResp.Status)
 	}
 
-	var result []byte
+	var respReader io.Reader
+
 	encoding := httpResp.Header.Get("Content-Encoding")
 	if encoding == "gzip" {
-		gzipContent, _ := gzip.NewReader(httpResp.Body)
-		result, err = io.ReadAll(gzipContent)
-		gzipContent.Close()
+		gzipReader := gzipReaderPool.Get().(*gzip.Reader)
+
+		defer func() {
+			gzipReader.Reset(bytes.NewReader(emptyGzipBytes)) // for safety sake, not required
+			gzipReaderPool.Put(gzipReader)
+		}()
+
+		err = gzipReader.Reset(httpResp.Body)
+		if err != nil {
+			requestFailed = true
+			log.Println("gzipReader.Reset(httpResp.Body) failed", err)
+			return nil, err
+		}
+		respReader = gzipReader
 	} else {
-		result, err = io.ReadAll(httpResp.Body)
+		respReader = httpResp.Body
 	}
-	if err != nil {
-		requestFailed = true
-		log.Println("Read Http Response.Body Failed:", err)
-		return nil, err
-	}
+
+	var bobbResp bobb.Response
 
 	if Debug {
-		log.Println("--- client receiving ---")
+		log.Println("--- DEBUG MODE ON > client receiving ---")
+		var resultBuffer bytes.Buffer
+		_, err = io.Copy(&resultBuffer, respReader)
+		if err != nil {
+			requestFailed = true
+			log.Println("Debug Mode, io.Copy from http respReader failed", err)
+			return nil, err
+		}
+		result := resultBuffer.Bytes()
 		log.Println(fmtJSON(result))
+		err = json.Unmarshal(result, &bobbResp)
+	} else {
+		err = json.NewDecoder(respReader).Decode(&bobbResp)
 	}
 
-	bobbResp := new(bobb.Response)
-	err = json.Unmarshal(result, bobbResp)
 	if err != nil {
-		log.Println("json.Unmarshal result into bobb.Response Failed:", err)
+		requestFailed = true
+		log.Println("json.Decode result into bobb.Response Failed:", err)
 		return nil, err
 	}
-	return bobbResp, err
+	return &bobbResp, err
 }
